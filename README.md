54ca772c9f34487c3289a1f22b606a7c89b35eca
Olena Hrechykhina <117777710+Elena-Hrechyk@users.noreply.github.com>
2023-08-02 17:11:52 -0700

    Summary (required)

---
+++ README.md
@@ -0,0 +1,1 @@
+скріншоти виконання команд [https://github.com/Elena-Hrechyk/goit-nodejs-hw/img](https://github.com/Elena-Hrechyk/goit-nodejs-hw/img)
--- db/contacts.js
+++ db/contacts.js
@@ -5,38 +5,54 @@ const { nanoid } = require("nanoid");
 const contactsPath = path.join(__dirname, "contacts.json");
 
 const listContacts = async () => {
-  const data = await fs.readFile(contactsPath);
-  return JSON.parse(data);
+  try {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+  } catch (err) {
+    console.log("Не вдалося прочитати файл contacts.json");
+  }
 };
 
 const getContactById = async (contactId) => {
-  const contacts = await listContacts();
-  const data = contacts.find((contact) => contact.id === contactId);
-  return data || null;
+  try {
+    const contacts = await listContacts();
+    const data = contacts.find((contact) => contact.id === contactId);
+    return data || null;
+  } catch (err) {
+    console.log("Не вдалося прочитати файл contacts.json");
+  }
 };
 
 const removeContact = async (contactId) => {
-  const contacts = await listContacts();
-  const indexDelContact = contacts.findIndex(
-    (contact) => contact.id === contactId
-  );
-  contacts.splice(indexDelContact, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  console.log(indexDelContact);
-  return indexDelContact;
+  try {
+    const contacts = await listContacts();
+    const indexDelContact = contacts.findIndex(
+      (contact) => contact.id === contactId
+    );
+    contacts.splice(indexDelContact, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    console.log(indexDelContact);
+    return indexDelContact;
+  } catch (err) {
+    console.log("Не вдалося перезаписати файл contacts.json");
+  }
 };
 
 const addContact = async ({ name, email, phone }) => {
-  const newContact = {
-    id: nanoid(),
-    name,
-    email,
-    phone,
-  };
-  const contacts = await listContacts();
-  contacts.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return newContact;
+  try {
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    const contacts = await listContacts();
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (err) {
+    console.log("Не вдалося перезаписати файл contacts.json");
+  }
 };
 
 module.exports = {
